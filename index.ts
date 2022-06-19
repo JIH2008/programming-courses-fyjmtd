03cd396d75a0cff046fde3cc312f6a664ee67922
JIH2008 <jihadmunjed2008@gmail.com>
2022-06-19 14:13:54 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let num1, num2, num3: number;
+let add: number;
+add = parseInt(prompt('this is an calcolator press 1 to try it'));
+
+num1 = parseInt(prompt('please add number 1'));
+num2 = parseInt(prompt('please add number 2'));
+num3 = num1 + num2;
+console.log(num3);
